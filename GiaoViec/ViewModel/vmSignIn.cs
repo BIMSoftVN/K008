65b3929d09d7ea3c870131e35dfc1940e225623b
Thế Anh Đỗ <theanhdo@live.com>
2024-09-05 22:59:39 +0700

    Đẩy lên

---
+++ GiaoViec/ViewModel/vmSignIn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiaoViec.ViewModel
+{
+    public class vmSignIn
+    {
+        public string Email { get; set; } = "anhdt@xuanmaicorp.vn";
+    }
+}
