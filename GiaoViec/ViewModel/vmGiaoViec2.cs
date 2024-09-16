bc1b2ac788443b63e48058ed9556d7857d1741aa
Thế Anh Đỗ <theanhdo@live.com>
2024-09-16 22:25:32 +0700

    Bài 8

---
+++ GiaoViec/ViewModel/vmGiaoViec2.cs
@@ -0,0 +1,68 @@
+﻿using GiaoViec.Libs;
+using GiaoViec.Views.Pages;
+using Microsoft.Xaml.Behaviors.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GiaoViec.ViewModel
+{
+    public class vmGiaoViec2 : PropertyChangedBase
+    {
+        private Page _MainFrameContent;
+        public Page MainFrameContent
+        {
+            get
+            {
+                return _MainFrameContent;
+            }
+            set
+            {
+                _MainFrameContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ActionCommand cmd_OpenMainPage;
+
+        public ICommand Cmd_OpenMainPage
+        {
+            get
+            {
+                if (cmd_OpenMainPage == null)
+                {
+                    cmd_OpenMainPage = new ActionCommand(PerformCmd_OpenMainPage);
+                }
+
+                return cmd_OpenMainPage;
+            }
+        }
+
+        private void PerformCmd_OpenMainPage(object parameter)
+        {
+            string ActionName = parameter as string;
+            try
+            {
+                switch (ActionName) 
+                {
+                    case "pGiaoViec":
+                        MainFrameContent = new pGiaoViec();
+                        break;
+
+                    case "pNguoiDung":
+                        MainFrameContent = new pNguoiDung();
+                        break;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ GiaoViec/Views/vGiaoViec2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Xpf.WindowsUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GiaoViec.Views
+{
+    /// <summary>
+    /// Interaction logic for vGiaoViec2.xaml
+    /// </summary>
+    public partial class vGiaoViec2 : Window
+    {
+        public vGiaoViec2()
+        {
+            InitializeComponent();
+        }
+
+        private void HamburgerMenuBottomBarNavigationButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var button = sender as HamburgerMenuBottomBarNavigationButton;
+            if (button !=null && button.ContextMenu !=null)
+            {
+                button.ContextMenu.PlacementTarget = button;
+                button.ContextMenu.IsOpen = true;
+            }    
+        }
+    }
+}
